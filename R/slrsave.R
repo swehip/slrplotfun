cccd9e26da320e610c3ec880678bd6df782da7b8
jon-nat <62013249+jon-nat@users.noreply.github.com>
2021-06-02 09:42:03 +0200

    Documentation added

--- R/slrsave.R
+++ R/slrsave.R
@@ -1,5 +1,13 @@
 #' ggsave function with default settings for SLR annual report
 #' 
+#' @param file File name to create on disk.
+#' @param plot Plot to save
+#' @param width Width in units ("in", "cm", or "mm").
+#' @param height Height in units ("in", "cm", or "mm").
+#' @param units Units ("in", "cm", or "mm")
+#' @param dpi Plot resolution.
+#' @param device Device to use. Can either be a device function (e.g. png()), or one of "eps", "ps", "tex" (pictex), "pdf", "jpeg", "tiff", "png", "bmp", "svg" or "wmf" (windows only).
+#' @param family Font family
 #' @export
 #' @import ggplot2
 #' 
