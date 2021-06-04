54e9a9d8d45025a3d53ea9d83cd19a34ed825e0b
Rikard Isaksson <rikard.isaksson@vgregion.se>
2021-06-04 07:12:21 +0200

    First working version of horisontal_bar_plot.R

--- R/bar_plot.R
+++ R/bar_plot.R
@@ -268,7 +268,7 @@ bar_plot <-
       ggplot2::scale_y_continuous(
         breaks = seq(0, y_breaks_end, by = y_breaks),
         limits = y_lim,
-        expand = if(expand) ggplot2::waiver() else c(0,0)
+        expand = if(expand) ggplot2::waiver() else c(0,0),
       )
   }
 
@@ -283,6 +283,21 @@ bar_plot <-
   }
 
   if(flip){
+    library(ggplot2)
+    # Helper functions for work-around with guides on discrete scale
+    guide_axis_label_trans <- function(label_trans = identity, ...) {
+      axis_guide <- guide_axis(...)
+      axis_guide$label_trans <- rlang::as_function(label_trans)
+      class(axis_guide) <- c("guide_axis_trans", class(axis_guide))
+      axis_guide
+    }
+
+    guide_train.guide_axis_trans <- function(x, ...) {
+      trained <- NextMethod()
+      trained$key$.label <- x$label_trans(trained$key$.label)
+      trained
+    }
+
     bars <-
       bars +
       ggplot2::coord_flip()
---
+++ R/horisontal_bar_plot.R
@@ -0,0 +1,101 @@
+# Function to make stacked horisontal bar diagram
+
+#' Bar plot function
+#'
+#' Plot a bar plot using ggplot2.
+#'
+#' @param df                 Data frame.
+#' @param ...                arguments passed to [theme_slr()]
+#' @param title              Plot title, `NULL` if no title.
+#' @param subtitle           Small text under title, `NULL` if no subtitle.
+#' @param y_lab              Y-axis label, use `NULL` for no label.
+#' @param x_lab              X-axis label, use `NULL` for no label.
+#'
+#' @return                   ggplot object containing bar plot.
+#'
+
+#' @export
+# Helper functions for work-around with guides on discrete scale
+guide_axis_label_trans <- function(label_trans = identity, ...){
+  axis_guide <- ggplot2::guide_axis(...)
+  axis_guide$label_trans <- rlang::as_function(label_trans)
+  class(axis_guide) <- c("guide_axis_trans", class(axis_guide))
+  axis_guide
+}
+
+#' @export
+guide_train.guide_axis_trans <- function(x, ...){
+  trained <- NextMethod()
+  trained$key$.label <- x$label_trans(trained$key$.label)
+  trained
+}
+
+#' @export
+horisontal_bar_plot <-
+  function(df,
+           title             = NULL,
+           subtitle          = NULL,
+           y_lab             = NULL,
+           x_lab             = NULL,
+           ...
+  )
+  {
+    df <-
+      df %>%
+      dplyr::group_by(lab, gen) %>%
+      dplyr::summarise(y = dplyr::n()) %>%
+      dplyr::group_by(lab) %>%
+      dplyr::mutate(y2 = y/sum(y)) %>%
+      dplyr::ungroup()
+
+    df$lab <- forcats::fct_reorder2(df$lab, df$gen, df$y2)
+
+
+    labe <-
+      df %>%
+      filter(gen == "Kvinna") %>%
+      mutate(
+        rlab = paste0(y, " (", round(y2,2)*100, "%)")
+      ) %>%
+      arrange(y2) %>%
+      .$rlab
+
+
+    #### create ggplot object ####
+    bars <-
+      ggplot2::ggplot(
+        df,
+        aes(x = lab,
+            y = y2,
+            fill = gen
+        )
+      ) +
+      ggplot2::ggtitle(
+        title,
+        subtitle = subtitle
+      ) +
+      ggplot2::labs(
+        x = x_lab,
+        y = y_lab
+      ) +
+      theme_slr(
+        subtitle = !is.null(subtitle),
+        x_lab_exists = !is.null(x_lab),
+        ...
+      ) +
+      ggplot2::geom_bar(
+        stat = "identity",
+        position = ggplot2::position_fill(vjust = 0.5,
+                                          reverse = TRUE)
+      ) +
+      ggplot2::scale_y_continuous(
+        expand = c(0,0)
+      ) +
+      ggplot2::guides(
+        y.sec = guide_axis_label_trans(~labe)
+      ) +
+      ggplot2::coord_flip()
+
+  return(bars)
+  }
+
