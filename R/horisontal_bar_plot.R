9c5960341b65d8ca44656aca64097079c2bbbac9
Rikard Isaksson <rikard.isaksson@vgregion.se>
2021-06-04 09:56:37 +0200

    Added comments and documentation

--- R/horisontal_bar_plot.R
+++ R/horisontal_bar_plot.R
@@ -5,24 +5,33 @@
 #' Plot a bar plot using ggplot2.
 #'
 #' @param df                 Data frame.
-#' @param ...                arguments passed to [theme_slr()]
+#' @param x_var              Variable for x axis, use string name.
+#'                           Recommended that `x_var` is in character in df.
+#' @param fill_var           Variable for the different colors in bars,
+#'                             use string name.
+#'                             Use `NULL` if only one color for bars.
 #' @param title              Plot title, `NULL` if no title.
 #' @param subtitle           Small text under title, `NULL` if no subtitle.
 #' @param y_lab              Y-axis label, use `NULL` for no label.
 #' @param x_lab              X-axis label, use `NULL` for no label.
+#' @param fill_colors        Color of the different categories in `fill_var`.
+#' @param percent_accuracy   Set accuracy for [scales::percent_format()].
+#' @param ...                arguments passed to [theme_slr()]
 #'
 #' @return                   ggplot object containing bar plot.
 #'
 
 #' @export
-# Helper functions for work-around with guides on discrete scale
+# Helper functions for work-around with guides on discrete scale.
+# This solution is presented in issue #3171 at
+# https://github.com/tidyverse/ggplot2/issues/3171
+# Credit to user paleolimbot for supplying this solution
 guide_axis_label_trans <- function(label_trans = identity, ...){
   axis_guide <- ggplot2::guide_axis(...)
   axis_guide$label_trans <- rlang::as_function(label_trans)
   class(axis_guide) <- c("guide_axis_trans", class(axis_guide))
   axis_guide
 }
-
 #' @export
 guide_train.guide_axis_trans <- function(x, ...){
   trained <- NextMethod()
@@ -49,7 +58,7 @@ horisontal_bar_plot <-
       n <- if (!is.null(fill_var)) length(unique(df[[fill_var]])) else NULL
       fill_colors <- slr_colors(n)
     }
-    #vars <- c(x_var, fill_var)
+    # Calculations by group
     df <-
       df %>%
       dplyr::group_by_at(
@@ -63,16 +72,17 @@ horisontal_bar_plot <-
       ) %>%
       dplyr::ungroup()
 
+    # Sort left-hand labels by y2
     df$lab <- forcats::fct_reorder2(df[[x_var]], df[[fill_var]], df$y2)
 
-
-    labe <-
+    # Construct right-hand labels sorted by y2
+    rlab <-
       df[df[[fill_var]] == levels(as.factor(df[[fill_var]]))[1], ] %>%
       dplyr::mutate(
           rlab = paste0(n, " (", round(y2,2)*100, "%)")
       ) %>%
-      arrange(y2) %>%
-      select(rlab)
+      dplyr::arrange(y2) %>%
+      dplyr::select(rlab)
 
     #### create ggplot object ####
     bars <-
@@ -109,7 +119,7 @@ horisontal_bar_plot <-
         expand = c(0,0)
       ) +
       ggplot2::guides(
-        y.sec = guide_axis_label_trans(~labe)
+        y.sec = guide_axis_label_trans(~rlab)
       ) +
       ggplot2::coord_flip()
 
