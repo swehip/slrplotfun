e37a981cfe088f4896040cd2e033d0b70543b6b6
Rikard Isaksson <rikard.isaksson@vgregion.se>
2021-08-24 15:30:11 +0200

    added helpers

--- R/horisontal_bar_plot.R
+++ R/horisontal_bar_plot.R
@@ -2,13 +2,14 @@
 # This solution is presented in issue #3171 at
 # https://github.com/tidyverse/ggplot2/issues/3171
 # Credit to user paleolimbot for supplying this solution
+#' @export
 guide_axis_label_trans <- function(label_trans = identity, ...){
   axis_guide <- ggplot2::guide_axis(...)
   axis_guide$label_trans <- rlang::as_function(label_trans)
   class(axis_guide) <- c("guide_axis_trans", class(axis_guide))
   axis_guide
 }
-
+#' @export
 guide_train.guide_axis_trans <- function(x, ...){
   trained <- NextMethod()
   trained$key$.label <- x$label_trans(trained$key$.label)
